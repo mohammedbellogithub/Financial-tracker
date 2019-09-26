90bd786dc852b82ea49e08244a211ddc90390fc0
Isaiah Osei-Mensah <oseimensahisaiah@gmail.com>
2019-09-26 13:45:05 +0100

    finished

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 app.use("/api/users/item", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/", express.static("client/dist"));
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
